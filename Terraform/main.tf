9420a704414cd9afbcddfdfbec178eaa2833591d
MichalADA <admaczykmichal@gmail.com>
2024-06-07 20:10:58 +0200

    editt terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,8 +5,16 @@ provider "aws" {
 resource "aws_instance" "apache_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  key_name      = var.key_name
   tags = {
     Name = "ApacheServer"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"  # lub inny użytkownik, np. ec2-user dla Amazon Linux
+    private_key = file(var.private_key_path)
+    host        = self.public_ip
   }
 
   provisioner "file" {
@@ -25,26 +33,41 @@ resource "aws_instance" "apache_server" {
     ]
   }
 
-  security_groups = [aws_security_group.allow_http.name]
+  security_groups = [aws_security_group.allow_http_https.name]
 }
 
 resource "aws_instance" "elk_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  key_name      = var.key_name
   tags = {
     Name = "ELKServer"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"  # lub inny użytkownik, np. ec2-user dla Amazon Linux
+    private_key = file(var.private_key_path)
+    host        = self.public_ip
   }
 
   security_groups = [aws_security_group.allow_elk.name]
 }
 
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
-  description = "Allow HTTP inbound traffic"
+resource "aws_security_group" "allow_http_https" {
+  name        = "allow_http_https"
+  description = "Allow HTTP and HTTPS inbound traffic"
 
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -7,11 +7,23 @@ variable "aws_region" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instances"
   type        = string
-  default     = "ami-0e001c9271cf7f3b9" # Przykładowa AMI
+  default     = "ami-0e001c9271cf7f3b9" # Aktualna AMI
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instances"
   type        = string
-  default     = "t2.medium"
+  default     = "t2.medium" # Aktualny typ instancji
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+  default     = "Elk-key"
+}
+
+variable "private_key_path" {
+  description = "Path to the private key file"
+  type        = string
+  default     = "Elk-key.pem"
 }
